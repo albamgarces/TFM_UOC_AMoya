d638a676f8621580670b2f3224b74091f563265a
albamgarces <garces@gmail.com>
2020-11-22 23:21:40 +0100

    subset.gbf parser with flags

--- ejercicios/input_parser.py
+++ ejercicios/input_parser.py
@@ -19,22 +19,23 @@ def extension(file_given):
     compt = {}
     compt["fasta"] = [".fa", ".mpfa", ".fna", ".fsa", ".fas", ".fasta"]
     compt["genbank"] = [".genbank", ".gb", ".gbf", ".gbff", ".gbk"]
+    compt["GFF"] = {".gff"}
     
-    print(compt)
-    print(compt["fasta"])
     #get file absolute path
     file_name_abs_path = os.path.abspath(file_given)
     #name_file, extension, path_file = os.path.splitext(file_name_abs_path)
     name_file, extension = os.path.splitext(file_name_abs_path)
     print(os.path.splitext(file_name_abs_path))
     
-    if extension in compt["genbank"]:
+    
+    if len(sys.argv)==3 and extension in compt["genbank"]:
         gbf_parser = protein_gbf.gbf_parser(file_given)
         
         for fasta in gbf_parser:
             protein_gbf.main(file_given)
-            
-        
+#     elif extension in compt[""]        
+    
+      
     else:
         print("")
         print("Sorry, doesn't work, try again!")
@@ -63,8 +64,8 @@ def main():
         print ("python %s annotation_file\n" %os.path.realpath(__file__))
         exit()        
 
-    ## llama a la funcion con el argumento 1
-    extension(sys.argv[1])   
+    
+    extension(sys.argv[2])   
 
     ## ATENCION: sys.argv[0] es el propio script de python!
     
@@ -76,17 +77,37 @@ if __name__ == '__main__':
 
     
 
-# def parseArgument():
-#     parser = ArgumentParser(description="Get proteins sequences from annotation file")
-#     parser.add_argument("-a", "--annot_file", metavar="", help="gb, gff or gtf annotation file")
-#     parser.add_argument("-r", "--ref_file", metavar="", help="Genome references FASTA file")
-#     parser.add_argument("-p", "--prot_file", metavar="", help="Protein sequence file")
-#     parser.add_argument("--debug", metavar="")
-#     parser.add_argument("-o", "--out_folder", metavar="", help="Results folder")
-#     return parser.parse_args()
-# 
-# arg= parseArgument()
+def parseArgument():
+    parser = ArgumentParser(description="Get proteins sequences from annotation file")
+    parser.add_argument("-a", "--annot_file", metavar="", help="Annotation file")
+    parser.add_argument("-r", "--ref_file", metavar="", help="Genome references FASTA file")
+    parser.add_argument("-p", "--prot_file", metavar="", help="Protein sequence file")
+    parser.add_argument("--debug", metavar="")
+    parser.add_argument("-o", "--out_folder", metavar="", help="Results folder")
+    return parser.parse_args()
+  
+arg= parseArgument()
+
+####################
+## starting
+####################
+
+
+if not arg.annot_file is None:
+    file_given = arg.annot_file
+    print("file given")
+    print(file_given)
+    print(" ")
+elif not arg.ref_file is None:
+    ref_file = arg.ref_file
+    print("ref file given")
+    print(ref_file)
+    print("")
+    
 # if arg.annot_file == None:
 #     print("Please provide an annotation file")
+else:
+    print("Please provide an annotation file")
+    
 
          
